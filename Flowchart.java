da4488a29df0419f5edee8a3cb23782fe6a2dced
Oäk Kingkaew <oakabc@gmail.com>
2024-01-27 10:22:42 +0700

    Add files via upload

---
+++ Flowchart.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author chanakarnkin
+ */
+public class Flowchart {
+    
+    public static void main(String[] args) {
+        
+        int x = 0;
+        
+        if (x > 10) {
+            System.out.println("Hello World");
+        }
+        else System.out.println("Hi There");
+        
+    }
+    
+}
